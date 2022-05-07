41a498a79bafccb230d6f00231a8f17a292ad796
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-05-07 16:45:54 +0600

    blog add

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -1,9 +1,64 @@
 import React from 'react';
 
+
 const Blog = () => {
     return (
-        <div>
-            this is blog
+        <div className='bg-gray-900 pt-16 pb-10 '>
+
+            <div className="max-w-4xl px-8 py-4 mx-auto  bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <div className="flex justify-center -mt-16 md:justify-end">
+                    <img className="object-cover w-20 h-20 border-2 border-gray-50 rounded-full " alt="Testimonial avatar"
+                        src="https://i.ibb.co/z6KcXNt/n-1.png" />
+                </div>
+
+                <h2 className="mt-2 text-2xl font-semibold text-amber-500  md:mt-0 md:text-3xl">
+                    Difference between javascript and nodejs
+                </h2>
+
+                <p className="mt-2 mb-5 text-gray-600 dark:text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+
+
+            </div>
+
+            <div className="max-w-4xl px-8 py-4 mx-auto mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <div className="flex justify-center -mt-16 md:justify-end">
+                    <img className="object-cover w-20 h-20 border-2 border-gray-50 rounded-full " alt="Testimonial avatar"
+                        src="https://i.ibb.co/Z2T59nD/n-2.png" />
+                </div>
+
+                <h2 className="mt-2 text-2xl font-semibold text-amber-500 md:mt-0 md:text-3xl">When should use nodejs and when should use mongodb</h2>
+
+                <p className="mt-2 mb-5 text-gray-600 dark:text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+
+
+            </div>
+
+
+            <div className="max-w-4xl px-8 py-4 mx-auto mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <div className="flex justify-center -mt-16 md:justify-end">
+                    <img className="object-cover w-20 h-20 border-2 border-gray-50 rounded-full " alt="Testimonial avatar"
+                        src="https://i.ibb.co/MsJFdPJ/n-3.png" />
+                </div>
+
+                <h2 className="mt-2 text-2xl font-semibold text-amber-500  md:mt-0 md:text-3xl">Differences between sql and nosql database</h2>
+
+                <p className="mt-2 mb-5 text-gray-600 dark:text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+
+
+            </div>
+
+            <div className="max-w-4xl px-8 py-4 mx-auto mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <div className="flex justify-center -mt-16 md:justify-end">
+                    <img className="object-cover w-20 h-20 border-2 border-gray-50 rounded-full " alt="Testimonial avatar"
+                        src="https://i.ibb.co/4mBkhxT/n-4.png" />
+                </div>
+
+                <h2 className="mt-2 text-2xl font-semibold text-amber-500  md:mt-0 md:text-3xl">What is the purpose of jwt and how it work</h2>
+
+                <p className="mt-2 mb-5 text-gray-600 dark:text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+
+
+            </div>
         </div>
     );
 };
