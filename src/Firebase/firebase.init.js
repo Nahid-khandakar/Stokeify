0335f7b24aa0f074a755890eda40affd243bc4b9
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-05-08 21:00:15 +0600

    add .env

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA0Spiw71KeyfDB546AeFfLfhYaDOPdrPM",
-    authDomain: "stokeify.firebaseapp.com",
-    projectId: "stokeify",
-    storageBucket: "stokeify.appspot.com",
-    messagingSenderId: "98229860084",
-    appId: "1:98229860084:web:1122219f6f0aa764fbd0a1"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
